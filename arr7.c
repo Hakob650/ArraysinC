e3b1a0532b336559cb865d3d5a5b5c46cd7c7d64
Hakob650 <hakob11meneshian@gmail.com>
2025-04-01 12:33:05 +0400

    Add files via upload

---
+++ arr7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int size=5;
+	int arr[size][size];
+	for(int i=0;i<size;++i){
+		for(int j=0;j<size;++j){
+			scanf("%d",&arr[i][j]);
+		}
+	}int sum=0;
+	for(int i=0;i<size;++i){
+		sum+=arr[i][i];
+        }printf("%d",sum);
+	return 0;
+}
