ac1c3709ef973c1667c5940b1581f24b50e52265
Hakob650 <hakob11meneshian@gmail.com>
2025-04-15 13:42:30 +0400

    Add files via upload

---
+++ homeworkApril14/findMaxConsecutiveOnes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int findMaxConsecutiveOnes(int* nums, int numsSize) {
+	   int max=0;
+	   int current=0;
+	   for(int i=0;i<numsSize;++i){
+		if(nums[i]==1){
+			current++;
+		     if(current>max){
+			max=current;
+		    }
+		}else{
+			current=0;
+	        }
+	   }return max;
+}
+
