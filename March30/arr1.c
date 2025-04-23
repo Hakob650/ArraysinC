155949dd1d1c174868a0227d16ef86d1b68c0bbe
Hakob650 <hakob11meneshian@gmail.com>
2025-04-23 12:20:49 +0400

    Update arr1.c

--- March30/arr1.c
+++ March30/arr1.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+	int size=5;
+	int arr[size];
+	for(int i=0;i<size;++i){
+		scanf("%d",&arr[i]);
+	}int min=arr[0];
+	for(int i=0;i<size;++i){
+		if(arr[i]<min){
+			min=arr[i];
+		}
+	}printf("%d",min);
+	return 0;
+}  
