1a122a7d3649873f66486070b0d8600eb7aab677
Hakob650 <hakob11meneshian@gmail.com>
2025-04-23 12:23:25 +0400

    Update arr3.c

--- March30/arr3.c
+++ March30/arr3.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+int main(){
+	int size=5;
+	int arr[size];
+	for(int i=0;i<size;++i){
+		scanf("%d",&arr[i]);
+	}int max=arr[0];
+	int index=0;
+	for(int i=0;i<size;++i){
+		if(arr[i]>max){
+			max=arr[i];
+			index=i;
+    		}
+	}printf("%d",index);
+	return 0;
+}
